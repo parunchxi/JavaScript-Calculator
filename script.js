a806287ca153f92c6506e9304e25821f1c1d0331
parunchxi <parunchxi@gmail.com>
2023-06-08 23:59:58 +0700

    Update theme

--- script.js
+++ script.js
@@ -141,3 +141,16 @@ if (theme === 'darkMode') {
 } else {
     setLightMode();
 }
+
+// Detect Theme
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    setDarkMode();
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    if (event.matches) {
+        setDarkMode();
+    } else {
+        setLightMode();
+    }
+});
